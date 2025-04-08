ca9533c6f2c94ffb0e22a6dd1a2314f2229d5dde
Gladkii Denis <gy@kode.ru>
2025-04-08 20:42:53 +0200

    feat(payment): lecture 1 livecoding

--- src/routing/app-navigation/app-navigation.tsx
+++ src/routing/app-navigation/app-navigation.tsx
@@ -1,3 +1,33 @@
+import { createStackNavigator } from '@react-navigation/stack'
+import { RootStackParamsList } from './types'
+import { PaymentCreate, PaymentServices } from '@screens/payment'
+import { rootStackOptions } from './config'
+import { HomeTabsNavigation } from '@routing/home-tabs-navigation'
+
+const RootStack = createStackNavigator<RootStackParamsList>()
+
 export const AppNavigation = () => {
-  return <></>
+  const isAuth = true
+
+  return (
+    <RootStack.Navigator
+      initialRouteName='paymentServices'
+      screenOptions={rootStackOptions}>
+      <RootStack.Screen
+        name='HomeTabs'
+        component={HomeTabsNavigation}
+        options={{ headerShown: false }}
+      />
+      <RootStack.Screen
+        name='paymentServices'
+        component={PaymentServices}
+        options={{ headerTitle: 'Мобильная связь' }}
+      />
+      <RootStack.Screen
+        name='paymentCreate'
+        component={PaymentCreate}
+        options={({ route }) => ({ headerTitle: route.params.title })}
+      />
+    </RootStack.Navigator>
+  )
 }
--- src/shared/ui/templates/keyboard-view/keyboard-view.tsx
+++ src/shared/ui/templates/keyboard-view/keyboard-view.tsx
@@ -1,28 +1,27 @@
-import { useHeaderHeight } from '@react-navigation/elements';
-import { KeyboardAvoidingView, Platform, StyleSheet } from 'react-native';
+import { useHeaderHeight } from '@react-navigation/elements'
+import { KeyboardAvoidingView, Platform, StyleSheet } from 'react-native'
 
 type TKeyboardViewProps = {
-  children: React.ReactNode;
-};
+  children: React.ReactNode
+}
 
 export const KeyboardView = ({ children }: TKeyboardViewProps) => {
-  const headerHeight = useHeaderHeight();
+  const headerHeight = useHeaderHeight()
 
   return (
     <KeyboardAvoidingView
       style={styles.root}
       contentContainerStyle={styles.root}
       enabled={Platform.OS == 'ios'}
-      behavior="padding"
-      keyboardVerticalOffset={headerHeight || 0}
-    >
+      behavior='padding'
+      keyboardVerticalOffset={headerHeight || 0}>
       {children}
     </KeyboardAvoidingView>
-  );
-};
+  )
+}
 
 const styles = StyleSheet.create({
   root: {
     flex: 1,
   },
-});
+})
